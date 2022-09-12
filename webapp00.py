312d3001e3c9a6a403cfa0b350f2bbce3d72d506
samaraterarrosa <113351545+samaraterarrosa@users.noreply.github.com>
2022-09-12 08:57:01 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -4,7 +4,7 @@
 import streamlit as st
   
 # Use st.title("") para adicionar um TÍTULO ao seu Web app
-st.title("Meu proffesor é incrivel")
+st.title("Meu professor é incrivel")
 
 # Use st.header("") para adicionar um CABEÇALHO ao seu Web app
 st.header("Cabeçalho")
